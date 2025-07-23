b618be27b69c7989a7c994710d31ce10909d8963
CodeMiner-掘金安东尼 <729922845@qq.com>
2025-07-23 22:18:19 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,13 @@
         </transition>
       </router-view>
     </main>
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <style>

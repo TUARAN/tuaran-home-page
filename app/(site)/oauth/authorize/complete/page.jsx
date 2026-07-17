ac6650609876027136752f6b69425c57d3d8fde5
CodeMiner-掘金安东尼 <729922845@qq.com>
2026-07-17 09:18:03 +0800

    Add weather MCP and OAuth success state

---
+++ app/(site)/oauth/authorize/complete/page.jsx
@@ -0,0 +1,7 @@
+import OAuthAuthorizationComplete from './OAuthAuthorizationComplete'
+
+export const metadata = { title: 'MCP 授权已完成', robots: { index: false, follow: false } }
+
+export default function OAuthAuthorizationCompletePage() {
+  return <OAuthAuthorizationComplete />
+}
